e44095a27743755fc45ea02c4238e5c91df07c1f
GiacomoVisconti <giacomovisconti1999@gmail.com>
2025-06-06 14:28:08 +0200

    milestone 1 complete

--- js/main.js
+++ js/main.js
@@ -1,13 +1,5 @@
-// Il programma dovrà chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero
-    //dichiaro le variabili per l'età e i km e ne prendo il dato dal form
-    const user_name_El = document.getElementById("user_full_name")
-    const user_km_El = document.getElementById('user_km_trip')
-    const user_age_El = document.getElementById()
-    
-
-// Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
-
-    // il prezzo del biglietto è definito in base ai km (0.21 € al km) e ne dichiaro la variabile 
+// !FUNCTION
+ // il prezzo del biglietto è definito in base ai km (0.21 € al km) e ne dichiaro la variabile 
     /**
      * ### This function returns the price of a ticket {number}.
      * - the parameter "user_age" is a selection between options identified by numbers:
@@ -22,27 +14,67 @@
         const price_for_km  = 0.21
         let ticket_price = user_km * price_for_km
 
-        if(user_age < 18){
+        if(user_age == 1){
 
             // va applicato uno sconto del 20% per i minorenni (età < 18)
             ticket_price = ticket_price - (ticket_price * 0.2)
     
-            // L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). Questo richiederà un minimo di ricerca.
-            alert(ticket_price.toLocaleString("en-US", {style:"currency", currency:"EUR"}));
+            // L'output 
+            return ticket_price
             
-        }else if(user_age > 65){
+        }else if(user_age == 3){
     
             // va applicato uno sconto del 40% per gli over 65. (età > 65)
             ticket_price = ticket_price - (ticket_price * 0.4)
     
-            // L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). Questo richiederà un minimo di ricerca.
-            alert(ticket_price.toLocaleString("en-US", {style:"currency", currency:"EUR"}));
-        }else{
+            // L'output 
+            return ticket_price
+        }else if (user_age == 2){
     
-            // L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). Questo richiederà un minimo di ricerca.
-            alert(ticket_price.toLocaleString("en-US", {style:"currency", currency:"EUR"}));
+            // L'output 
+            return ticket_price
         }
     }
+
+// !END FUNCTION
+
+// Il programma dovrà chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero
+    //dichiaro le variabili per l'età e i km e ne prendo il dato dal form
+    const user_name_El = document.getElementById("user_full_name")
+    const user_km_El = document.getElementById('user_km_trip')
+    const user_age_El = document.getElementById('user_age')
+    const button_sub_El = document.getElementById('btn_submit_data')
+    const form_El = document.querySelector('form')
+
+    form_El.addEventListener('submit', (e) => {
+        //Disable the reload of the page
+        e.preventDefault();
+    })
+
+    button_sub_El.addEventListener('click', () => {
+        
+        console.log('ciao');
+
+        //Declaring the constants for the input datas
+        const user_age_value = user_age_El.value 
+        const user_km_value = user_km_El.value
+        const user_name_value = user_name_El.value
+        const 
+        
+
+        console.log(user_age_value, user_km_value, user_name_value);
+        
+
+
+
+    })
+
+
+    
+
+
+
+
     
 
     
